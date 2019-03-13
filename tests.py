182d4d669dd0a332dbeba0f7a08b4f5d883aa205
DanielTakeshi <takeshidanny@gmail.com>
2019-03-13 12:21:40 -0700

    timing observation, 4x speed up with just type defs

--- tests.py
+++ tests.py
@@ -12,13 +12,25 @@ from example       import primes
 def test(n):
     """With n=10k, I get:
 
-    (py2-cython-tests) Daniels-MBP-6:cython-tests danielseita$ python tests.py
-    python: 2.321
-    python compiled: 1.221
-    cythonized compiled: 0.002
+        (py2-cython-tests) Daniels-MBP-6:cython-tests danielseita$ python tests.py
+        python: 2.321
+        python compiled: 1.221
+        cythonized compiled: 0.002
 
     It's consistent across several runs, though I know time.time() is probably
     less accurate than timeit.
+
+    Also, if you change `example_py_cy.py` to `example_py_cy.pyx` and only add a
+    bunch of `cdef int`s in methods, and `int`s to the function arguments, we
+    get:
+
+        (py2-cython-tests) Daniels-MBP-6:cython-tests danielseita$ python tests.py
+        python: 2.393
+        python compiled: 0.308
+        cythonized compiled: 0.002
+
+    So, I get a 4x speed-up even with just simple cdef int's ... that's odd but
+    also promising for our code since it means there's low-hanging fruit.
     """
     tic = time.time()
     primes_python(n)
